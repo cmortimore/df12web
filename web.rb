d345f8ab4c0625d1b7456d85d7f164cb476f9f76
Chuck Mortimore <cmortimore@salesforce.com>
2012-09-07 16:47:46 -0700

    env

--- web.rb
+++ web.rb
@@ -5,7 +5,7 @@ $client_secret = ENV['client_secret']
 $redirect_uri = ENV['redirect_uri']
 
 get '/' do
-  "<a href='https://login.salesforce.com/services/oauth2/authorize?response_type=code&client_id=3MVG99OxTyEMCQ3gNp2PjkqeZKwQqMOOxLzAKtPhEwpD.HEMVGcw9sLXfYOmLG3U_G_JOxd6MbpwmMmj0VDQj&client_secret=930847565453470131&redirect_uri=https%3A%2F%2Fdf12web.herokuapp.com%2Foauth'>login with salesforce</a>"
+  "<a href='https://login.salesforce.com/services/oauth2/authorize?response_type=code&client_id=" + $client_id + "&client_secret=" + $client_secret + "&redirect_uri=" + $redirect_uri + "'>login with salesforce</a>"
 end
 
 get '/auth' do
