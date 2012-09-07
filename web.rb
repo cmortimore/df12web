5bc9412a1922c1a46bc75215dd0abbca5cc020b2
Chuck Mortimore <cmortimore@salesforce.com>
2012-09-07 16:41:47 -0700

    login url

--- web.rb
+++ web.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+$client_id = ENV['client_id']
+$client_secret = ENV['client_secret']
+$redirect_uri = ENV['redirect_uri']
+
 get '/' do
-  "Hello, world"
+  "<a href='https://login.salesforce.com/services/oauth2/authorize?response_type=code&client_id=3MVG99OxTyEMCQ3gNp2PjkqeZKwQqMOOxLzAKtPhEwpD.HEMVGcw9sLXfYOmLG3U_G_JOxd6MbpwmMmj0VDQj&client_secret=930847565453470131&redirect_uri=https%3A%2F%2Fdf12web.herokuapp.com%2Foauth'>login with salesforce</a>"
 end
