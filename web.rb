cec4ec86739a8ce6f8581ff93a7d7d8f41090245
Chuck Mortimore <cmortimore@salesforce.com>
2012-09-07 17:44:09 -0700

    simpler

--- web.rb
+++ web.rb
@@ -18,7 +18,9 @@ get '/oauth' do
     http.use_ssl = true
     path = '/services/oauth2/token'
     data = 'client_id=' + $client_id + '&client_secret=' + $client_secret + '&redirect_uri=' + $redirect_uri + '&grant_type=authorization_code&code=' + code
+    headers = { 'Content-Type' => 'application/x-www-form-urlencoded' }
     resp, data = http.post(path, data, headers)
+    
     "Token Response:  #{resp.body}"
   end
 end
