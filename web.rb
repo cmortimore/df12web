6024a8df91b867c3948bca29c77f2bd8258a141f
Chuck Mortimore <cmortimore@salesforce.com>
2012-09-07 17:28:27 -0700

    debug

--- web.rb
+++ web.rb
@@ -14,24 +14,16 @@ end
 get '/oauth' do
   code = params[:code]
   unless code.nil?
-    #http = Net::HTTP.new('login.salesforce.com', 443)
-    #http.use_ssl = true
-    #path = '/services/oauth2/token'
-    http = Net::HTTP.new('requestb.in', 80)
-    path = 'oje8usoj'
-    data = 'serwis=wp.pl&url=profil.html&tryLogin=1&countTest=1&logowaniessl=1&login_username=blah&login_password=blah'
+    http = Net::HTTP.new('login.salesforce.com', 443)
+    http.use_ssl = true
+    path = '/services/oauth2/token'
+    #http = Net::HTTP.new('requestb.in', 80)
+    #path = 'oje8usoj'
+    'client_id=' + $client_id + '&client_secret=' + $client_secret + '&redirect_uri=' + $redirect_uri + '&grant_type=authorization_code&code=' + code
+    puts "data:" + data
     headers = { 'Content-Type' => 'application/x-www-form-urlencoded' }
     resp, data = http.post(path, data, headers)
     
-    
-    #request.set_form_data({"client_id" => $client_id, "client_secret" => $client_secret, "redirect_uri" => $redirect_uri, "grant_type" => "authorization_code", "code" => code})
-    #headers = {'Content-Type'=> 'application/x-www-form-urlencoded'}
-    #response = http.request(request)
-    
-    
-    #data = 'client_id=' + $client_id + '&client_secret=' + $client_secret + '&redirect_uri=' + $redirect_uri + '&grant_type=authorization_code&code=' + code
-    #headers = {'Content-Type'=> 'application/x-www-form-urlencoded'}
-    #resp, data = http.post(path, data, headers)
     "Token Reponse: #{resp}"
   end
 end
