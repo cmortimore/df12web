a7e77af3b7eb245ace8f7194390afdf9ef87ac0c
Chuck Mortimore <cmortimore@salesforce.com>
2012-09-07 17:08:55 -0700

    simpler

--- web.rb
+++ web.rb
@@ -8,13 +8,19 @@ $client_secret = ENV['client_secret']
 $redirect_uri = ENV['redirect_uri']
 
 get '/' do
-  "<a href='https://login.salesforce.com/services/oauth2/authorize?response_type=code&client_id=" + $client_id + "&client_secret=" + $client_secret + "&redirect_uri=" + $redirect_uri + "'>login with salesforce</a>"
+  '<a href="https://login.salesforce.com/services/oauth2/authorize?response_type=code&client_id=' + $client_id + '&client_secret=' + $client_secret + '&redirect_uri=' + $redirect_uri + '">login with salesforce</a>'
 end
 
 get '/oauth' do
   code = params[:code]
   unless code.nil?
-    "Code #{code}!"
+    http = Net::HTTP.new('https://login.salesforce.com', 443)
+    http.use_ssl = true
+    path = '/services/oauth2/token'
+    data = 'client_id=' + $client_id + '&client_secret=' + $client_secret + '&redirect_uri=' + $redirect_uri + '&grant_type=authorization_code&code=' + code
+    headers = {'Content-Type'=> 'application/x-www-form-urlencoded'}
+    resp, data = http.post(path, data, headers)
+    "Token Reponse: #{resp.message}"
   end
 end
 
