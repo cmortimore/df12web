42531b60cecb2154b1342797434a639bfe907976
Chuck Mortimore <cmortimore@salesforce.com>
2012-09-07 17:41:21 -0700

    simpler

--- web.rb
+++ web.rb
@@ -17,13 +17,8 @@ get '/oauth' do
     http = Net::HTTP.new('login.salesforce.com', 443)
     http.use_ssl = true
     path = '/services/oauth2/token'
-    #http = Net::HTTP.new('requestb.in', 80)
-    #path = 'oje8usoj'
     data = 'client_id=' + $client_id + '&client_secret=' + $client_secret + '&redirect_uri=' + $redirect_uri + '&grant_type=authorization_code&code=' + code
-    puts "data:" + data
-    headers = { 'Content-Type' => 'application/x-www-form-urlencoded' }
     resp, data = http.post(path, data, headers)
-    
     "Token Response:  #{resp.body}"
   end
 end
