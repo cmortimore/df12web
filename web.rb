c158081105fbb7a9576fa86d3045c60e12c3d2c0
Chuck Mortimore <cmortimore@salesforce.com>
2012-09-07 17:12:50 -0700

    debug

--- web.rb
+++ web.rb
@@ -14,9 +14,11 @@ end
 get '/oauth' do
   code = params[:code]
   unless code.nil?
-    http = Net::HTTP.new('login.salesforce.com', 443)
-    http.use_ssl = true
-    path = '/services/oauth2/token'
+    #http = Net::HTTP.new('login.salesforce.com', 443)
+    #http.use_ssl = true
+    #path = '/services/oauth2/token'
+    http = Net::HTTP.new('requestb.in', 80)
+    path = 'oje8usoj'
     data = 'client_id=' + $client_id + '&client_secret=' + $client_secret + '&redirect_uri=' + $redirect_uri + '&grant_type=authorization_code&code=' + code
     headers = {'Content-Type'=> 'application/x-www-form-urlencoded'}
     resp, data = http.post(path, data, headers)
